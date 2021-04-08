4466161ad20a512ba25b8b8464141a52ffd9469d
gavin <gavin.haines@yell.com>
2021-04-08 19:02:41 +0100

    template project build

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,7 @@
 import kotlinx.browser.document
 
 fun main() {
-    document.write("Bingo!")
+    document.write("Bingo!" +
+            "\n" +
+            "That was absolutely the right thing to do")
 }
