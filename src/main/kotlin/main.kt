4499495587e9af76076c2452bcacd6e61d16ff15
gavin <gavin.haines@yell.com>
2021-04-09 15:51:37 +0100

    Font

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,13 @@
 import kotlinx.browser.document
+import kotlinx.html.dom.append
+import kotlinx.html.js.*
 
 fun main() {
-    document.write("That was absolutely the right thing to do")
+
+    document.getElementById("app")
+        ?.also { it.innerHTML = "" }
+        ?.append {
+            h1 { +"That was absolutely the right thing to do" }
+        }
+    document.write("Bingo!")
 }
